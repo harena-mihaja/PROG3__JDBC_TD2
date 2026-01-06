2fcc7ffe4c0b72b21b2d90aac8000755a64b817a
harena-mihaja <mihaja2506@gmail.com>
2026-01-06 06:33:21 +0300

    fix: correct "id" to use serial

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,17 +1,19 @@
 CREATE TYPE dish_type AS ENUM ('START', 'MAIN', 'DESSERT');
 CREATE TYPE category AS ENUM ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
 
-CREATE TABLE dish (
-    id int primary key,
-    name varchar(255) not null,
-    dish_type dish_type not null
+CREATE TABLE dish
+(
+    id        serial primary key,
+    name      varchar(255) not null,
+    dish_type dish_type    not null
 );
 
-CREATE TABLE ingredient(
-    id int primary key ,
-    name varchar(255) not null ,
-    price numeric(10,2) not null ,
-    category category not null,
-    id_dish int,
-    constraint dish_fk foreign key (id_dish) references dish(id)
+CREATE TABLE ingredient
+(
+    id       serial primary key,
+    name     varchar(255)   not null,
+    price    numeric(10, 2) not null,
+    category category       not null,
+    id_dish  int,
+    constraint dish_fk foreign key (id_dish) references dish (id)
 );
